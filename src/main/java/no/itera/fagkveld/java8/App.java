b6393ff411472a42010a2572ed8c139be0ad935d
Vegard Haugstvedt <it.vegard@gmail.com>
2014-11-24 03:33:15 +0100

    Initial commit

---
+++ src/main/java/no/itera/fagkveld/java8/App.java
@@ -0,0 +1,13 @@
+package no.itera.fagkveld.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
