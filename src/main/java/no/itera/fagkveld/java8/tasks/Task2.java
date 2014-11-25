52fbc931e61811b59262934f2db229f7eac6369a
Vegard Haugstvedt <it.vegard@gmail.com>
2014-11-25 20:13:32 +0100

    Added task descriptions

--- src/main/java/no/itera/fagkveld/java8/tasks/Task2.java
+++ src/main/java/no/itera/fagkveld/java8/tasks/Task2.java
@@ -2,20 +2,37 @@ package no.itera.fagkveld.java8.tasks;
 
 import no.itera.fagkveld.java8.api.soccer.Team;
 
+/*
+ * In this class, use no.itera.fagkveld.java8.api.soccer.Tippeligaen.getTeams() to get the collection to base the Stream on
+ */
 public class Task2 {
 	
+	/*
+	 * Return the average number of goals made in Tippeligaen 2014
+	 */
 	public static double getAverageNumberOfGoalsScoredInSeason() {
 		return Double.MIN_VALUE;
 	}
 	
+	/*
+	 * Return the total number of goals made in Tippeligaen 2014
+	 */
 	public static int getTotalNumberOfGoalsScored() {
 		return Integer.MIN_VALUE;
 	}
 	
+	/*
+	 * Return the winner of Tippeligaen 2014.
+	 * 
+	 * Hint: The winner is the Team with the most points
+	 */
 	public static Team getGoldWinner() {
 		return null;
 	}
 	
+	/*
+	 * Return the names of the teams in (ascending) alphabetical order.
+	 */
 	public static Object[] getTeamsInAlphabeticalOrder() {
 		return null;
 	}
--- src/main/java/no/itera/fagkveld/java8/tasks/Task3.java
+++ src/main/java/no/itera/fagkveld/java8/tasks/Task3.java
@@ -3,7 +3,9 @@ package no.itera.fagkveld.java8.tasks;
 public class Task3 {
 
 	/*
-	 * Return an array of the names of all files (and directories - for simplicity)
+	 * Return an array of the names of all files (and directories - for simplicity) in the directory the path specifies.
+	 * 
+	 * Hint: Child directories, etc. should not be included.
 	 */
 	public static Object[] getFileNamesInDirectory(String path) {
 		return null;
